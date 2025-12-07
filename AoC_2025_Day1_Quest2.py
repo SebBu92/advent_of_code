59f5c403f6e853574bb838dd55a69860cd7a85ab
Sebastian Buchner <sebbel9@freenet.de>
2025-12-07 12:26:50 +0100

    day1_quest2_finalVersion

--- AoC_2025_Day1_Quest2.py
+++ AoC_2025_Day1_Quest2.py
@@ -4,21 +4,16 @@ with open("aoc_quest1.txt", "r") as quest:
     for values in quest: # textdatei öffnen
         single_value = values.split() # zeilenumbruch der werte in der txt
         for value in single_value: # jede zeile als eigener string
-            if value[0] == "L":
-                number -= int(value[1:])
-                if number == 0 or number == 100:
+            for every_value in range(int(value[1:])):
+                if value[0] == "R":
+                    number += 1
+                else:
+                    number -= 1
+                
+                if number == -1 or number == 100:
+                    number %= 100
+
+                if number == 0:
                     counter += 1
-                if (number + 100) < 0:
-                    while number < 0:
-                        number += 100
-                        counter += 1
-            if value[0] == "R":
-                number += int(value[1:])
-                if number == 0 or number == 100:
-                    counter += 1
-                if (number - 100) > 100:
-                    while number > 100:
-                        counter += 1
-                        number -= 100
 print(f"Counter: {counter}")
 
